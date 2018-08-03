c84c67319c61f279c2d95a3e90347a6c0d655666
Laboratory Fortune <eric.fortune@njit.edu>
2018-08-03 15:24:42 -0400

    / ‘GattusoMotion/ISIfinder.m’

--- GattusoMotion/ISIfinder.m
+++ GattusoMotion/ISIfinder.m
@@ -1,13 +1,15 @@
 function ISI = ISIfinder(neuron)
 ISI = [];
 size = input('size for ISI');
-for jj = 1:length(neuron)
-    if neuron(jj).sizeDX == size
-    ISI = [ISI diff(neuron(jj).st)']; 
-    end
-end
 binsize = input('binsize');
 cutoff = input('ISI cutoff');
+for kk = 1:length(neuron)
+    for jj = 1:length(neuron(kk).s)
+        if neuron(kk).s(jj).sizeDX == size
+        ISI = [ISI diff(neuron(kk).s(jj).st)']; 
+        end
+    end
+end
 fredrico = 0:binsize:cutoff;
 figure;
 a = histogram(ISI, 'BinEdges', fredrico);
