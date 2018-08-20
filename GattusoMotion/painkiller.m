550211870ae6d22a96405b1f463361fdf9420bab
Hannah Gattuso <hannahchristine18@gmail.com>
2018-08-20 15:21:09 -0400

    + ‘GattusoMotion/painkiller.m’

---
+++ GattusoMotion/painkiller.m
@@ -0,0 +1,25 @@
+function out = painkiller(data)
+    %% calculating the ISI
+    ISI = ISIfinder(data);
+    
+    %% creating histograms and heatmaps
+    hotstuff = sunburn(data);
+    
+    % in order to call data forms that are made in the creation of the
+    % heatmaps and histograms, call out.heathist and you have the options
+    % posvel, tmpPV, accvel, tmpAV, pos, vel, and acc
+    
+     %% calculating DSI and nDSI (must come after the heat and histo because need the output of the heatmaps)
+     DSI = killerDSI(hotstuff);
+     nDSI = objDSI(hotstuff);  %%this will be done for the size that is selected for the heatmaps and histograms
+     
+     %% completing the STA
+     spiketa = sta(hotstuff.spikes, hotstuff.objpos, hotstuff.Fs); 
+     
+     
+out.ISI = ISI;
+out.hotstuff = hotstuff;
+out.DSI = DSI;
+out.nDSI = nDSI;
+out.sta = spiketa;
+end 
