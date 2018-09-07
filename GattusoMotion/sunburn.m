5c98746ee511cb84ef4ce73712eeceae31bf5414
Hannah Gattuso <hannahchristine18@gmail.com>
2018-09-07 09:53:03 -0400

    / ‘GattusoMotion/sunburn.m’

--- GattusoMotion/sunburn.m
+++ GattusoMotion/sunburn.m
@@ -96,9 +96,9 @@ figure;
 velsteps = 0.0005;
 accsteps = 0.0000015;
 posedges = -5:0.1:5;
-veledges = min(out.vel):velsteps:max(out.vel); % need to define bins rather than calculate them on a per trial basis 
+veledges = -0.2:velsteps:0.2; % need to define bins rather than calculate them on a per trial basis 
 
-accedges = min(out.acc):accsteps:max(out.acc); % need to define bins rather than calculate them on a per trial basis 
+accedges = -0.1:accsteps:0.1; % need to define bins rather than calculate them on a per trial basis 
 
 subplot(131); a = histcounts(out.pos, posedges); plot(posedges(1:end-1), a/sum(a), 'b'), 
 subplot(132); b = histcounts(out.vel, veledges); plot(veledges(1:end-1), b/sum(b), 'b'),
