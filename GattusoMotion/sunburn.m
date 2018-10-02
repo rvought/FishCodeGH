3473e6adcdbfc53e1ba144c95e01165462a8f527
Laboratory Fortune <eric.fortune@njit.edu>
2018-10-02 15:45:26 -0400

    / ‘GattusoMotion/sunburn.m’

--- GattusoMotion/sunburn.m
+++ GattusoMotion/sunburn.m
@@ -101,7 +101,8 @@ veledges = -0.02:velsteps:0.02; % need to define bins rather than calculate them
 accedges = -0.25*10^(-4):accsteps:0.25*10^(-4); % need to define bins rather than calculate them on a per trial basis 
 
 for zz = 1:length(out.acc)
-    if out.acc(z) 
+    if out.acc(z) > 0
+        
     
 subplot(231); a = histcounts(out.pos, posedges); plot(posedges(1:end-1), a/sum(a), 'b'), 
 subplot(232); b = histcounts(out.vel, veledges); plot(veledges(1:end-1), b/sum(b), 'b'),
