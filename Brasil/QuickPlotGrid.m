fa7fa4c08ea4cb0e517bb3e3ae482c0cef1814a7
Eric Fortune <eric.fortune@njit.edu>
2018-11-08 13:49:59 -0500

    + ‘Brasil/QuickPlotGrid.m’

---
+++ Brasil/QuickPlotGrid.m
@@ -0,0 +1,11 @@
+function QuickPlotGrid(in, win)
+
+   figure(1); clf; hold on;
+   figure(2); clf; hold on;
+   
+for j = 1:length(in.fish)
+    
+   figure(1); plot(in.t), in.fish(j).freq), '*'); 
+   figure(2); plot(in.fish(j).x, in.fish.y, '*-');
+    
+end
