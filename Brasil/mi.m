980312a9ed288cfc25bee5070a7c19bad33242a0
Eric Fortune <eric.fortune@njit.edu>
2018-12-14 12:11:34 -0500

    / ‘Brasil/mi.m’

--- Brasil/mi.m
+++ Brasil/mi.m
@@ -29,5 +29,5 @@ I=sum(minf(n2values, na'*nb));
 
 % -----------------------
 function y=minf(pab,papb)
-I=find(papb > 1e-12 & pab > 1e-12); % function support 
-y=pab(I).*log2(pab(I)./papb(I));
+I = find(papb > 1e-12 & pab > 1e-12); % function support 
+y = pab(I) .* log2(pab(I) ./ papb(I));
