b5b2beea088ac3f36f9c0caf96d8283d5a92ce91
Eric Fortune <eric.fortune@njit.edu>
2018-12-14 14:51:12 -0500

    / ‘Brasil/bCorr.m’

--- Brasil/bCorr.m
+++ Brasil/bCorr.m
@@ -147,7 +147,7 @@ for j=1:length(data) % For each recording session
                 curdistrack = fillmissing(Descartes, 'linear') - mean(fillmissing(Descartes, 'linear'));
                 curdFs = fillmissing(dF, 'linear') - mean(fillmissing(dF, 'linear'));                
                
-                for kk = 1:stepz-1
+                for kk = 1:stepz
 
                     curridx = sharedidx(sharedtims > startim & sharedtims < startim+analtime);
                     
