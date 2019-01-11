3d69c23534dc69b9da409224a3ad6dca9e1b479e
Eric Fortune <eric.fortune@njit.edu>
2019-01-11 13:24:14 -0500

    / ‘Brasil/quickgridplot.m’

--- Brasil/quickgridplot.m
+++ Brasil/quickgridplot.m
@@ -23,7 +23,7 @@ end
 figure(1); clf; hold on; ylim(freqrange);
 figure(2); clf; hold on;
 wp=get(gcf,'Position');
-set(gcf, 'Position', [wp(1),wp(2),200,200])
+set(gcf, 'Position', [wp(1),wp(2),400,400])
 
 for j=1:length(in.fish)
     
