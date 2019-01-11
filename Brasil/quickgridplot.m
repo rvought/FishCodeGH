067647947f354d5887e1e34b8895b3709ed732fe
Eric Fortune <eric.fortune@njit.edu>
2019-01-11 12:47:09 -0500

    / ‘Brasil/quickgridplot.m’

--- Brasil/quickgridplot.m
+++ Brasil/quickgridplot.m
@@ -1,4 +1,10 @@
 function out = quickgridplot(in, freqrange, timrange)
 % Usage: out = quickgridplot(in, freqrange, timrange)
-% This plots frequency above position of fish in a grid
+% This plots frequency and position of fish in a grid recording
+
+
+
+% Frequency plot
+figure(1); clf;
+
 
