a67914ccbd92dac2ff16e6d4c3d46dacdd4caf96
Eric Fortune <eric.fortune@gmail.com>
2019-01-27 17:02:05 -0500

    / ‘Brasil/QuickPlotGrid.m’

--- Brasil/QuickPlotGrid.m
+++ Brasil/QuickPlotGrid.m
@@ -4,8 +4,9 @@ function QuickPlotGrid(in)
    figure(2); clf; hold on;
    
 for j = 1:length(in.fish)
-    
+   
+   tt = find(~isnan(in.fish(j).freq(:,2)));
    figure(1); plot(in.fish(j).freq(:,1), in.fish(j).freq(:,2), '*'); ylim([200 600]);
-   figure(2); plot(in.fish(j).x, in.fish(j).y, '*-');
+   figure(2); plot(in.fish(j).x(tt), in.fish(j).y(tt), '*-');
     
 end
