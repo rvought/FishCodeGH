e71ecc7324ddd9889cd019733fd86fd3f62107d1
Eric Fortune <eric.fortune@gmail.com>
2019-01-27 17:16:47 -0500

    / ‘Brasil/QuickPlotGrid.m’

--- Brasil/QuickPlotGrid.m
+++ Brasil/QuickPlotGrid.m
@@ -1,8 +1,13 @@
 function QuickPlotGrid(in, freqs, tims)
-nargin
+% Usage: QuickPlotGrid(in, freqs, tims)
+% where freqs and tims are optional. 
+% Freqs in Hz, 
+
 if nargin == 1 
-    freqs = [200 600];
+    freqs = [200 600]; % Default frequency range for EODs
 end    
+
+freqs = sort(freqs);
 
    figure(1); clf; hold on;
    figure(2); clf; hold on;
@@ -12,12 +17,14 @@ for j = 1:length(in.fish)
         tt = find(~isnan(in.fish(j).freq(:,2))); % all valid data
         
         if nargin == 3 % use only data in the range specified by the user
+            tims = sort(tims);
             uu = find(in.fish(j).freq(:,1) > tims(1) & in.fish(j).freq(:,1) < tims(2));
             tt = intersect(tt, uu);
         end
    
    
    figure(1); plot(in.fish(j).freq(tt,1), in.fish(j).freq(tt,2), '.', 'MarkerSize', 8); ylim(freqs);
-   figure(2); plot(in.fish(j).x(tt), in.fish(j).y(tt), '.', 'MarkerSize', 8); axis([-200, 150, -100, 250]);
+   figure(2); plot(in.fish(j).x(tt), in.fish(j).y(tt), '.', 'MarkerSize', 8); 
+        axis([-200, 150, -100, 250]);
     
 end
