83bd512990630e9baf059147d275898d4a701d87
Eric Fortune <eric.fortune@njit.edu>
2019-01-28 10:02:27 -0500

    / ‘Brasil/Daphne2019/SpaceCorps.m’

--- Brasil/Daphne2019/SpaceCorps.m
+++ Brasil/Daphne2019/SpaceCorps.m
@@ -9,7 +9,7 @@ function out = SpaceCorps(in)
 numfish = length(in.fish); % How many fish in this recording
 
 for j = numfish:-1:1 % For each pair of fish
-    F1(1,:) = in.fish(combos(j,1)).x(~isnan(in.fish(combos(j,1)).freq(:,2)));
+    F1(1,:) = in.fish(j).x(~isnan(in.fish(j).freq(:,2)));
     out(j).f1h = hist3(F1', ctrs);
     clear F1
 end
