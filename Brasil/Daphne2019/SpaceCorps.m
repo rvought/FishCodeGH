0fccbbc4ea16e2231063a9852d4f2213c6ed9436
Eric Fortune <eric.fortune@njit.edu>
2019-01-28 09:56:21 -0500

    / ‘Brasil/Daphne2019/SpaceCorps.m’

--- Brasil/Daphne2019/SpaceCorps.m
+++ Brasil/Daphne2019/SpaceCorps.m
@@ -19,7 +19,7 @@ for p = length(combos):-1:1 % For each pair of fish
     % Need to get only data points for which we have a valid frequency reading
     ttf1 = find(~isnan(in.fish(combos(p,1)).freq(:,2))); % First fish of combo
     ttf2 = find(~isnan(in.fish(combos(p,2)).freq(:,2))); % Second fish of combo
-
+combos(p,1)
     F1(1,:) = in.fish(combos(p,1)).x(ttf1);
     F1(2,:) = in.fish(combos(p,1)).y(ttf1);
 
