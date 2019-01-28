774303d85f29f42b0bb5747f259df37b33715854
Eric Fortune <eric.fortune@njit.edu>
2019-01-28 10:01:59 -0500

    / ‘Brasil/Daphne2019/SpaceCorps.m’

--- Brasil/Daphne2019/SpaceCorps.m
+++ Brasil/Daphne2019/SpaceCorps.m
@@ -8,28 +8,20 @@ function out = SpaceCorps(in)
 
 numfish = length(in.fish); % How many fish in this recording
 
-if numfish > 1 % We have more than one fish
-    
-combos = combnk(1:numfish, 2); % All pairwise combinations of fish
-
-for p = length(combos):-1:1 % For each pair of fish
-
-    out(p).fishnums = combos(p,:); % Save the output combo
-
-    % Need to get only data points for which we have a valid frequency reading
-    ttf1 = find(~isnan(in.fish(combos(p,1)).freq(:,2))); % First fish of combo
-    ttf2 = find(~isnan(in.fish(combos(p,2)).freq(:,2))); % Second fish of combo
-
-    F1(1,:) = in.fish(combos(p,1)).x(ttf1);
-    F1(2,:) = in.fish(combos(p,1)).y(ttf1);
-
-    F2(1,:) = in.fish(combos(p,2)).x(ttf2);
-    F2(2,:) = in.fish(combos(p,2)).y(ttf2);
-
-    out(p).f1h = hist3(F1', ctrs);
-    out(p).f2h = hist3(F2', ctrs);
-
-    clear F1 F2    
+for j = numfish:-1:1 % For each pair of fish
+    F1(1,:) = in.fish(combos(p,1)).x(~isnan(in.fish(combos(p,1)).freq(:,2)));
+    out(j).f1h = hist3(F1', ctrs);
+    clear F1
 end
 
-end
+% if numfish > 1 % We have more than one fish
+%     
+% combos = combnk(1:numfish, 2); % All pairwise combinations of fish
+% 
+% for p = length(combos):-1:1 % For each pair of fish
+% 
+%     out(p).fishnums = combos(p,:); % Save the output combo
+% 
+% end
+% 
+% end
