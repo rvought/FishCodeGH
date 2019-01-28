fb2c19f90a0976ed103161baf289b16a33056caa
Eric Fortune <eric.fortune@njit.edu>
2019-01-28 09:39:14 -0500

    / ‘Brasil/Daphne2019/SpaceCorps.m’

--- Brasil/Daphne2019/SpaceCorps.m
+++ Brasil/Daphne2019/SpaceCorps.m
@@ -1,2 +1,19 @@
 function out = SpaceCorps(in)
 
+
+F1(1,:) = in.fish(2).x;
+F1(2,:) = in.fish(2).y;
+
+F2(1,:) = in.fish(3).x;
+F2(2,:) = in.fish(3).y;
+
+ctrs(:,1) = -250:10:250;
+ctrs(:,2) = -250:10:250;
+
+f1h = hist3(F1, ctrs);
+f2h = hist3(F2, ctrs);
+
+figure(1); clf; 
+subplot(121); surf(f1h); view(0,90);
+subplot(122); surf(f1h); view(0,90);
+
