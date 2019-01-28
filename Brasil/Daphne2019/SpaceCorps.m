14ecfd3b0025b893495387b8a11d9e0d148842ef
Eric Fortune <eric.fortune@njit.edu>
2019-01-28 09:52:30 -0500

    / ‘Brasil/Daphne2019/SpaceCorps.m’

--- Brasil/Daphne2019/SpaceCorps.m
+++ Brasil/Daphne2019/SpaceCorps.m
@@ -3,28 +3,28 @@ function out = SpaceCorps(in)
 % Find spatial relations between fish
 % Distance histograms, range overlap
 
+    ctrs{1} = -150:5:250;
+    ctrs{2} = -150:5:250;
+
 
 combos = combnk(1:numfish, 2); % All pairwise combinations of fish
 
+for p = length(combos):-1:1 % For each pair of fish
 
-ttf2 = find(~isnan(in.fish(2).freq(:,2)));
-ttf3 = find(~isnan(in.fish(3).freq(:,2)));
+    out(p).fishnums = combos(p,:); % Save the output combo
 
-F1(1,:) = in.fish(2).x(ttf2);
-F1(2,:) = in.fish(2).y(ttf2);
+    % Need to get only data points for which we have a valid frequency reading
+    ttf1 = find(~isnan(in.fish(combos(p,1)).freq(:,2))); % First fish of combo
+    ttf2 = find(~isnan(in.fish(combos(p,2)).freq(:,2))); % Second fish of combo
 
-F2(1,:) = in.fish(3).x(ttf3);
-F2(2,:) = in.fish(3).y(ttf3);
+    F1(1,:) = in.fish(combos(p,1)).x(ttf1);
+    F1(2,:) = in.fish(combos(p,1)).y(ttf1);
 
-ctrs{1} = -150:5:250;
-ctrs{2} = -150:5:250;
+    F2(1,:) = in.fish(combos(p,2)).x(ttf2);
+    F2(2,:) = in.fish(combos(p,2)).y(ttf2);
 
-f1h = hist3(F1', ctrs);
-f2h = hist3(F2', ctrs);
-
-figure(2); clf; 
-subplot(121); surf(f1h); view(0,90);
-subplot(122); surf(f2h); view(0,90);
-
-out = 1;
+    out(p).f1h = hist3(F1', ctrs);
+    out(p).f2h = hist3(F2', ctrs);
+    
+end
 
