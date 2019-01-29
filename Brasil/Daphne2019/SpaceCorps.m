5e2fbc46593a6445d88a1972002eee92c32680b5
Eric Fortune <eric.fortune@njit.edu>
2019-01-29 08:26:19 -0500

    / ‘Brasil/Daphne2019/SpaceCorps.m’

--- Brasil/Daphne2019/SpaceCorps.m
+++ Brasil/Daphne2019/SpaceCorps.m
@@ -8,9 +8,10 @@ function [out, cmbs] = SpaceCorps(in)
 
 numfish = length(in.fish); % How many fish in this recording
 
-for j = numfish:-1:1 % For each pair of fish
-    tmp(j).xy(1,:) = in.fish(j).x(~isnan(in.fish(j).freq(:,2)));
-    tmp(j).xy(2,:) = in.fish(j).y(~isnan(in.fish(j).freq(:,2)));    
+for j = numfish:-1:1 % For each fish
+    tmp(j).valididx = find(~isnan(in.fish(j).freq(:,2)));
+    tmp(j).xy(1,:) = in.fish(j).x(tmp(j).valididx);
+    tmp(j).xy(2,:) = in.fish(j).y(tmp(j).valididx);  
     out(j).f1h = hist3(tmp(j).xy', ctrs);
 end
 
@@ -21,7 +22,8 @@ if numfish > 1 % We have more than one fish
     for p = length(combos):-1:1 % For each pair of fish
 
         cmbs(p).fishnums = combos(p,:); % Save the output combo
-        cmbs(p).dist = pdist2(tmp(combos(p,1)).xy, tmp(combos(p,2)).xy);
+        tt = 
+        cmbs(p).dist = pdist2(tmp(combos(p,1)).xy(tt), tmp(combos(p,2)).xy(tt));
 
     end
 
