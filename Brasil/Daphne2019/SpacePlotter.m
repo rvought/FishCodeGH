719df22ecf4a321240ff2ba563cdde5e36ec8090
Eric Fortune <eric.fortune@njit.edu>
2019-02-03 12:44:54 -0500

    / ‘Brasil/Daphne2019/SpacePlotter.m’

--- Brasil/Daphne2019/SpacePlotter.m
+++ Brasil/Daphne2019/SpacePlotter.m
@@ -7,9 +7,9 @@ subplot(222); surf(in(num).randhist); view(90,-90)
 
 
 subplot(223); plot(in(num).realXY(1,:), in(num).realXY(2,:), '.', 'MarkerSize', 8); 
-    xlim([xminedge-20, xminedge+xscale+20]); ylim([yminedge-20, yminedge+yscale+20]);
+    xlim([in(1).xbounds(1)-20, in(1).xbounds(2)+20]); ylim([in(1).ybounds(1)-20, in(1).ybounds(2)+20]);
 subplot(224); plot(in(num).rndXY(1,:), in(num).rndXY(2,:), '.', 'MarkerSize', 8); 
-    xlim([xminedge-20, xminedge+xscale+20]); ylim([yminedge-20, yminedge+yscale+20]);
+    xlim([in(1).xbounds(1)-20, in(1).xbounds(2)+20]); ylim([in(1).ybounds(1)-20, in(1).ybounds(2)+20]);
 
     
     
