d03c666186b2bb1a040859b388d5d04b266fc7db
Eric Fortune <eric.fortune@njit.edu>
2019-02-09 17:50:42 -0500

    / ‘Brasil/Daphne2019/overture.m’

--- Brasil/Daphne2019/overture.m
+++ Brasil/Daphne2019/overture.m
@@ -10,23 +10,22 @@ end
 
 out(1).overlap = []; out(1).overfish = [];
 
-    combos = combnk(fishlist, 2)
-    % All pairwise combinations of fish
-
+    combos = combnk(fishlist, 2); % All pairwise combinations of fish
+    
     for p = numfish:-1:1 % For each fish
 
-        for n = length(combos) % For each pair of fish
-
-            if ~isempty(find([combos(:,n)] == p, 1))
-                
-            out(p).combo(n).diffhist = in(fishlist(combos(1,n))).realhist - in(fishlist(combos(2,n))).realhist;
+        for n = 1:length(combos) % For each pair of fish
             
-            if fishlist(combos(1,n)) == p
-                out(p).overlap(end+1) = 1 - (sum(out(p).combo(n).diffhist(out(p).combo(n).diffhist > 0)) / sum(sum(in(fishlist(combos(1,n))).realhist)));
-                out(p).overfish(end+1) = fishlist(combos(2,n));
+            if ~isempty(find(combos(n,:) == p, 1))
+                
+            out(p).combo(n).diffhist = in(fishlist(combos(n,1))).realhist - in(fishlist(combos(n,2))).realhist;
+            
+            if fishlist(combos(n,1)) == p
+                out(p).overlap(end+1) = 1 - (sum(out(p).combo(n).diffhist(out(p).combo(n).diffhist > 0)) / sum(sum(in(fishlist(combos(n,1))).realhist)));
+                out(p).overfish(end+1) = fishlist(combos(n,2));
             else
-                out(p).overlap(end+1) = 1 - abs((sum(out(p).combo(n).diffhist(out(p).combo(n).diffhist < 0)) / sum(sum(in(fishlist(combos(2,n))).realhist))));
-                out(p).overfish(end+1) = fishlist(combos(1,n));
+                out(p).overlap(end+1) = 1 - abs((sum(out(p).combo(n).diffhist(out(p).combo(n).diffhist < 0)) / sum(sum(in(fishlist(combos(n,2))).realhist))));
+                out(p).overfish(end+1) = fishlist(combos(n,1));
             end
             
             end
