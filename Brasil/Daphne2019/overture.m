21fe585b954aa2e4b53d050c17a7d0c868eaf38f
Eric Fortune <eric.fortune@njit.edu>
2019-02-09 12:31:23 -0500

    / ‘Brasil/Daphne2019/overture.m’

--- Brasil/Daphne2019/overture.m
+++ Brasil/Daphne2019/overture.m
@@ -1,9 +1,22 @@
-function out = overture(in)
+function out = overture(in, fishlist)
 % Calculates the overlap in movements for all pairs of fish
+
+numfish = length(fishlist);
+
+    combos = combnk(1:numfish, 2); % All pairwise combinations of fish
+
+    for p = length(fishlist):-1:1 % For each pair of fish
+
 
 
 diffhist = in(1).realhist - in(2).realhist;
 
 overlapfishone = 1 - (sum(diffhist(diffhist > 0)) / sum(sum(in(1).realhist)));
 overlapfishtwo = 1 - (sum(diffhist(diffhist < 0)) / sum(sum(in(2).realhist)));
+
+
+
+    end
+    
+    
 
