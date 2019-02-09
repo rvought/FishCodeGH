c50b9e158dd8f9893a9972c16334db4dfc11c0c1
Eric Fortune <eric.fortune@njit.edu>
2019-02-09 17:35:27 -0500

    / ‘Brasil/Daphne2019/overture.m’

--- Brasil/Daphne2019/overture.m
+++ Brasil/Daphne2019/overture.m
@@ -7,19 +7,25 @@ else
    numfish = length(in);
    fishlist = 1:numfish;
 end
-    combos = combnk(1:numfish, 2); % All pairwise combinations of fish
 
-    for p = length(fishlist):-1:1 % For each pair of fish
+    combos = combnk(fishlist, 2); % All pairwise combinations of fish
 
+    for p = numfish:-1:1 % For each fish
 
+        for n = length(combos) % For each pair of fish
 
-diffhist = in(1).realhist - in(2).realhist;
-
-overlapfishone = 1 - (sum(diffhist(diffhist > 0)) / sum(sum(in(1).realhist)));
-overlapfishtwo = 1 - (sum(diffhist(diffhist < 0)) / sum(sum(in(2).realhist)));
-
-
-
+            if ~isempty(combos(:,n) == p)
+                
+            out(p).combo(n).diffhist = in(fishlist(combos(1,n))).realhist - in(fishlist(combos(1,n))).realhist;
+            
+            if fishlist(combos(1,n) == p
+                overlapfishone = 1 - (sum(diffhist(diffhist > 0)) / sum(sum(in(1).realhist)));
+            else
+            overlapfishtwo = 1 - (sum(diffhist(diffhist < 0)) / sum(sum(in(2).realhist)));
+            end
+            
+            end
+        end
     end
     
     
