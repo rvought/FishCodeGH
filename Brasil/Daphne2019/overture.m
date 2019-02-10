fed2282c0427d73848530ac41101d9c3f3d31f44
Eric Fortune <eric.fortune@njit.edu>
2019-02-10 01:04:19 -0500

    / ‘Brasil/Daphne2019/overture.m’

--- Brasil/Daphne2019/overture.m
+++ Brasil/Daphne2019/overture.m
@@ -10,15 +10,18 @@ end
 
 out(1).overlap = []; out(1).overfish = [];
 
-    combos = combnk(fishlist, 2); % All pairwise combinations of fish
+combos = combnk(fishlist, 2); % All pairwise combinations of fish
     
     for p = numfish:-1:1 % For each fish
 
         for n = 1:length(combos) % For each pair of fish
             
-            if ~isempty(find(combos(n,:) == p, 1))
-                combos(n,:)
-            out(p).combo(n).diffhist = in(fishlist(combos(n,1))).realhist - in(fishlist(combos(n,2))).realhist;
+            if ~isempty(find(combos(n,:) == p, 1)) % Use only cases with focal fish in it (p)
+
+                % Need a check to ensure input is not empty and that
+                % matrices have same dimensions
+
+                out(p).combo(n).diffhist = in(fishlist(combos(n,1))).realhist - in(fishlist(combos(n,2))).realhist;
             
             if fishlist(combos(n,1)) == p
                 out(p).overlap(end+1) = 1 - (sum(out(p).combo(n).diffhist(out(p).combo(n).diffhist > 0)) / sum(sum(in(fishlist(combos(n,1))).realhist)));
