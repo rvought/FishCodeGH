ef620ee9f9f04ec6475c7abee6a1ecc8036971af
Eric Fortune <eric.fortune@njit.edu>
2019-03-30 12:50:31 -0400

    / ‘DistDfAnalysis/dist_df_plots.m’

--- DistDfAnalysis/dist_df_plots.m
+++ DistDfAnalysis/dist_df_plots.m
@@ -665,7 +665,7 @@ subplot(2,1,1);
 hold on;
 
 histogram(R_rand,100);
-% plot(R_ddist_df_abs_cave(2,1),0,'*r');
+% plot(R_ddist_df_abs_cave(2,1),0,'*r'); %% ESF ERROR CHANGE
 
 legend('','Cave R value');
 title('Shuffled R values for Cave fish');
@@ -687,7 +687,7 @@ subplot(2,1,2);
 hold on;
 
 histogram(R_rand,100);
-% plot(R_ddist_df_abs_cave(2,1),0,'*r');
+% plot(R_ddist_df_abs_cave(2,1),0,'*r');  %% ESF ERROR CHANGE
 
 legend('','Surface R value');
 title('Shuffled R values for Surface fish');
