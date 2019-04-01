e9b65d208b91f8c739e9abd8675030fb098f8ddc
Eric Fortune <eric.fortune@njit.edu>
2019-04-01 07:52:19 -0400

    / ‘Brasil/Daphne2019/bFishAmpComparo.m’

--- Brasil/Daphne2019/bFishAmpComparo.m
+++ Brasil/Daphne2019/bFishAmpComparo.m
@@ -26,16 +26,18 @@ stts.stdCaveAmp = std(CaveAmps(CaveAmps < OutlierLevel));
 % [stts.H,stts.P,stts.CI,stts.STATS] = ttest2(CaveAmps(CaveAmps < OutlierLevel), SurfaceAmps(SurfaceAmps <OutlierLevel));
 [stts.H,stts.P,stts.CI,stts.STATS] = ttest2(CaveAmps, SurfaceAmps);
 
+    figure(1); clf; subplot(211); plot(CaveAmps, '.', 'MarkerSize', 2); hold on; plot(SurfaceAmps, '.', 'MarkerSize', 2);
+
     fprintf('Amplitudes different between cave and surface pVal = %1.4f \n', stts.P);
     fprintf('Cave mean & std %1.4f %1.4f \n', stts.meanCaveAmp*100, stts.stdCaveAmp*100);
     fprintf('Surface mean & std %1.4f %1.4f \n', stts.meanSurfaceAmp*100, stts.stdSurfaceAmp*100);
 
 
-figure(1); clf;
+figure(1); subplot(212); hold on;
     ctrs = 0:0.0001:0.004;
-    ax(1) = subplot(211); histogram(CaveAmps(CaveAmps < OutlierLevel),ctrs); 
+    histogram(CaveAmps(CaveAmps < OutlierLevel),ctrs); 
         text(0.002,20, 'CaveFish EOD dipole Strengths');
-    ax(2) = subplot(212); histogram(SurfaceAmps(SurfaceAmps < OutlierLevel), ctrs);
+    histogram(SurfaceAmps(SurfaceAmps < OutlierLevel), ctrs);
         text(0.002,20, 'Surface Fish EOD dipole Strengths');
     linkaxes(ax, 'xy');
     xlim([0 0.004]);
