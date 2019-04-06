7b285889f51fc4a589f63dbcca5db2b98827ba7b
Eric Fortune <eric.fortune@njit.edu>
2019-04-06 11:44:56 -0400

    / ‘DistDfAnalysis/dist_df_plots.m’

--- DistDfAnalysis/dist_df_plots.m
+++ DistDfAnalysis/dist_df_plots.m
@@ -159,8 +159,8 @@ timeIdx = dat.t>time(1) & dat.t<time(2);
 
 figure(1); clf, hold on;
 
-plot(dat.t(timeIdx), dat.fish(C(pair,1)).freq(timeIdx,2));
-plot(dat.t(timeIdx), dat.fish(C(pair,2)).freq(timeIdx,2));
+plot(dat.t(timeIdx), dat.fish(C(pair,1)).freq(timeIdx,2), 'LineWidth', 2);
+plot(dat.t(timeIdx), dat.fish(C(pair,2)).freq(timeIdx,2), 'LineWidth', 2);
 
 title('Single low-df example in cave fish, crossing pair');
 xlabel('Time (s)');
