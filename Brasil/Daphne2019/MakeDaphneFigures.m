7533c1ad5a135f623b1557c4d0521750778d38b3
Eric Fortune <eric.fortune@njit.edu>
2019-05-01 09:54:18 -0400

    / ‘Brasil/Daphne2019/MakeDaphneFigures.m’

--- Brasil/Daphne2019/MakeDaphneFigures.m
+++ Brasil/Daphne2019/MakeDaphneFigures.m
@@ -2,4 +2,4 @@
 %% Example plots for fish movement versus frequency
 
     QuickPlotGrid(cave(12), [], [250 450], [0 600]); figure(2); axis([-75, 175, -200, 50]);
-    QuickPlotGrid(srf(5), [], [250, 450], [0 600]); figure(2); axis([-145, 105, -125, 125]);
+%    QuickPlotGrid(srf(5), [], [250, 450], [0 600]); figure(2); axis([-145, 105, -125, 125]);
