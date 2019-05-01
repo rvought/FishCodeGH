4abddc0c5d336d5e9b8116e25566dc301794df64
Eric Fortune <eric.fortune@njit.edu>
2019-05-01 18:19:05 -0400

    Another day

--- Brasil/Daphne2019/MakeDaphneFigures.m
+++ Brasil/Daphne2019/MakeDaphneFigures.m
@@ -4,5 +4,18 @@
 
 %% Example plots for fish movement versus frequency
 
-    QuickPlotGrid(cave(12), [], [250 450], [0 600]); figure(2); axis([-75, 175, -200, 50]);
-%    QuickPlotGrid(srf(5), [], [250, 450], [0 600]); figure(2); axis([-145, 105, -125, 125]);
+    QuickPlotGrid(cave(12), [], [250 450], [240 360]); figure(2); axis([-75, 175, -200, 50]);
+    pause(1);
+    saveas(1,'caveFreqs.eps', 'epsc'); saveas(2,'cavePositions.eps', 'epsc');
+    pause(1);
+    QuickPlotGrid(srf(5), [], [250, 450], [120 240]); figure(2); axis([-145, 105, -125, 125]);
+    pause(1);
+    saveas(1,'surFreqs.eps', 'epsc'); saveas(2,'surfPositions.eps', 'epsc');
+    pause(1);
+
+    
+%% EOD Amplitudes
+
+
+
+
