e0e27f589ec23ade6e9955c4b411a3ddb972a020
Eric Fortune <eric.fortune@njit.edu>
2019-05-03 14:27:59 -0400

    / ‘Brasil/Daphne2019/bFishAmpComparo.m’

--- Brasil/Daphne2019/bFishAmpComparo.m
+++ Brasil/Daphne2019/bFishAmpComparo.m
@@ -39,8 +39,8 @@ stts.stdCaveAmp = std(CaveAmps);
 
 figure(1); subplot(212); hold on;
     ctrs = 0:0.003/30:0.003;
-    histogram(SurfaceAmps(GoodIDXs), ctrs, 'FaceColor', '[20 100 20]');
-    histogram(CaveAmps, ctrs, 'FaceColor', '[100 0 0]'); 
+    histogram(SurfaceAmps(GoodIDXs), ctrs, 'FaceColor', '[0 1 0]');
+    histogram(CaveAmps, ctrs, 'FaceColor', '[1 0 0]'); 
 %    linkaxes(ax, 'xy');
     xlim([0 0.003]);
 
