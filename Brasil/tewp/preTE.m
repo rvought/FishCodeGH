a0ebded77a4e2213b7c89644cd1b9c1dbf8449fe
Eric Fortune <eric.fortune@gmail.com>
2019-05-30 14:40:24 -0400

    / ‘Brasil/tewp/preTE.m’

--- Brasil/tewp/preTE.m
+++ Brasil/tewp/preTE.m
@@ -8,7 +8,7 @@ fishnum = length(fishidx); % How many fish
 combos = combnk(fishidx, 2); % Each pair of fish that we plan to analyze
 
 Fs = 1/mean(data.fish(fishidx(1)).freq(:,1));
-[b,a] = butter(3, 1/(Fs/2));
+[b,a] = butter(3, 1/(Fs/2), 'low');
 
 %% Copy original data into structure
 for k = fishnum:-1:1
