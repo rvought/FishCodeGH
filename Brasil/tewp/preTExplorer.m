59e90a54da7ae0a9e11b48cccd18046437c554da
Eric Fortune <eric.fortune@njit.edu>
2019-06-01 13:48:18 -0400

    / ‘Brasil/tewp/preTExplorer.m’

--- Brasil/tewp/preTExplorer.m
+++ Brasil/tewp/preTExplorer.m
@@ -45,8 +45,8 @@ for j=1:length(dFdist)
         
         [CC, TT] = slideCorr(tmpdF, tmpDistance, tims, windw, advanceby);
         
-        plot(TT, CC); xlim([0, max(tims)]); ylim([-1, 1]);
-        text(100, 0, 
+        plot(TT, CC); xlim([0, max(tims)]); ylim([-1, 1], 'k-o');
+        text(100, 0, num2str(mean(CC)));
 
 %     xa = xcorr(tmpdF, tmpDistance);
 %     plot(xa); xlim([0 length(xa)]);
