def38815165372919949406e2d8c4bd6afb7ce07
Eric Fortune <eric.fortune@njit.edu>
2019-06-02 16:00:18 -0400

    + ‘Brasil/tewp/SampleList.m’

---
+++ Brasil/tewp/SampleList.m
@@ -0,0 +1,70 @@
+%% Cave 3
+
+% All good data 0-1200
+%10 might be a good video for interaction
+%19 Best every POS case 0-1200
+%21 excellent NEG case from 600-1200
+
+%% Cave 4
+% 1-4 0-1200 good
+
+%5 400-1200 (ALL CASES WITH FISH 6)
+%10 400-1200
+%14 400-1200
+%17 400-1200
+%19 400-1200
+%21 400-1200
+
+% Fish 5 up to 1000 (4,9,13,16,19,20)
+
+%% Cave 5
+% All good data 0-1200
+
+%% Cave 6
+
+%Fish 2 up to 1000 (1,8,9,10,11,12,13,20)
+%Fish 5 100-1100 (5,11,16,20,23,26,27) hmmm
+
+%6 down shifts anti JAR? FISH 6
+%7 fish 1 and 8 chirps lead to distance increase or distance decrease leads to chirps.
+%13 good NEG corr throughout
+%14 No JAR response!!
+
+%% Cave 7
+
+%Sample 0-900
+%Fish 2 150-900 (1,7,8,9,10,11
+%Fish 6 0-400 (5,10,14,17,19,21
+
+%% Cave 8
+
+%Sample 0-1250
+%Fish2 1100-1250 (1,10,11,12,13,14,15,16,17
+%25,31 Reasonable NO JAR cases
+
+%% Cave 10 
+
+% Total 3 fish, 0-900 All good
+
+%% Cave 11
+
+%Sample 0-900
+%12 Reasonable NO JAR case
+
+
+%% Surface 3
+
+%1 200-500
+%2 0-600
+%3 75-175
+%4 200-600
+%5 
+%6 450-600
+%7 275-600
+%8 225-425
+%9 450-600
+%10 
+%11 0-250
+%- ERROR
+
+
