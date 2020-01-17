9bf7a84c1e4e58add0666c447b0ee8e568ded4e0
Eric Fortune <eric.fortune@gmail.com>
2020-01-17 10:33:48 -0500

    / ‘Brasil/Daphne2019/runDfC.m’

--- Brasil/Daphne2019/runDfC.m
+++ Brasil/Daphne2019/runDfC.m
@@ -23,7 +23,7 @@ figure(1); clf; hold on;
 
     
 % Low dFs histogram
-numbins = 22; 
+numbins = 10; 
 maxfreq = 10;   
 ctrs = 0:maxfreq/numbins:maxfreq;
 plotspots = ctrs(2:end) - maxfreq/(2*numbins);
