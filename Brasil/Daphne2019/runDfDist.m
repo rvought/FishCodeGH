1e75dd1442930bd858c6a9cfcc8b0750c8a9d208
Eric Fortune <eric.fortune@gmail.com>
2020-01-20 12:30:17 -0500

    + ‘Brasil/Daphne2019/runDfDist.m’

---
+++ Brasil/Daphne2019/runDfDist.m
@@ -0,0 +1,7 @@
+% This script produces figure 5 for the manuscript -  interactions
+% between dF and distance fish in cave and surface habitats. Relies on dFanalysis.m
+
+% Calculate CAVE dFs TIME CONSUMING - ONLY DO THIS ONCE prior to running this script!
+% load SurfaceDataRev2018a.mat; load CaveDataRev2018a.mat;
+% [caveDF, CalldFs] = dFanalysis(cave);
+% [srfDF, SalldFs] = dFanalysis(srf);
