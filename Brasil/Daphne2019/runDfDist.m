05a6c9a606ff6da5043fdf015587d9edcdc5948a
Eric Fortune <eric.fortune@njit.edu>
2020-01-21 14:12:28 -0500

    / ‘Brasil/Daphne2019/runDfDist.m’

--- Brasil/Daphne2019/runDfDist.m
+++ Brasil/Daphne2019/runDfDist.m
@@ -83,7 +83,7 @@ cenbins = -1:0.2:1;
 % ax(2) = subplot(312); histogram(shuffCorrs, cenbins);
 % ax(3) = subplot(313); histogram(shiftCorrs, cenbins);
 % linkaxes(ax, 'xy'); xlim([-1,1]);
-histogram(realCorrs, cenbins); hold on; histogram(shuffCorrs, cenbins);
+histogram(shuffCorrs, cenbins); hold on; histogram(realCorrs, cenbins);
 
 clear stepnum tt tf ts fakies z tim makethemthesamelength numbins j kk cenbins
 
