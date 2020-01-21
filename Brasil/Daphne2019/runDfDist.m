88d7aaaea78cae5bf95323c2f26ec5b1619da5dc
Eric Fortune <eric.fortune@njit.edu>
2020-01-21 12:07:43 -0500

    / ‘Brasil/Daphne2019/runDfDist.m’

--- Brasil/Daphne2019/runDfDist.m
+++ Brasil/Daphne2019/runDfDist.m
@@ -110,7 +110,7 @@ plot(tim, caveDF(kval).pair(pval).descartes, '.');
 yyaxis right; plot(tim, caveDF(kval).pair(pval).dF, '.');
 plot(tim, filtfilt(b,a, caveDF(kval).pair(pval).dF), '-k');
 
-% Plot extreme examples - this is correlation coefficient of -0.9256
+% Plot extreme examples - this is correlation coefficient of -XXXX
 kval = 5; pval = 21;
 tim = 1/Fs:1/Fs:length(caveDF(kval).pair(pval).descartes)/Fs;
 figure(3); clf; hold on;
