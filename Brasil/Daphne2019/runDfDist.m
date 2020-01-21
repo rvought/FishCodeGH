49edb37f3c7302b510263407c8f2917662dc3842
Eric Fortune <eric.fortune@njit.edu>
2020-01-21 11:43:44 -0500

    / ‘Brasil/Daphne2019/runDfDist.m’

--- Brasil/Daphne2019/runDfDist.m
+++ Brasil/Daphne2019/runDfDist.m
@@ -102,6 +102,9 @@ clear stepnum CorrWindow StepSz tt tf ts fakies z tim makethemthesamelength numb
 tim = 1/Fs:1/Fs:length(caveDF(6).pair(1).descartes)/Fs;
 figure(2); clf; plot(tim, caveDF(6).pair(1).descartes); yyaxis right; plot(tim, caveDF(6).pair(1).dF);
 
+% Plot extreme examples - this is correlation coefficient of -0.9048
+tim = 1/Fs:1/Fs:length(caveDF(3).pair(1).descartes)/Fs;
+figure(3); clf; plot(tim, caveDF(3).pair(1).descartes); yyaxis right; plot(tim, caveDF(3).pair(1).dF);
 
 
 
