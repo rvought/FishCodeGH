a9c895f89a696740a6d649267648f99d6806515f
Eric Fortune <eric.fortune@njit.edu>
2020-01-21 10:21:28 -0500

    / ‘Brasil/Daphne2019/runDfDist.m’

--- Brasil/Daphne2019/runDfDist.m
+++ Brasil/Daphne2019/runDfDist.m
@@ -5,8 +5,11 @@
 % load SurfaceDataRev2018a.mat; load CaveDataRev2018a.mat;
 % [caveDF, CalldFs] = dFanalysis(cave);
 % [srfDF, SalldFs] = dFanalysis(srf);
-realCorrs = []; fakeCorrs = [];
+
+realCorrs = []; shuffCorrs = []; shiftCorrs;
+
 Fs = 4.8828;
+
 CorrWindow = 300; % Time in seconds for correlation analysis
 StepSz = 150; % Time in seconds for the step
 
@@ -26,17 +29,30 @@ for j = 1:length(data(kk).pair) % For each pair of fish
     clear tmp;
     
     for z = 1:stepnum
-        tt = find(tim > StepSz * (z-1) & tim < (StepSz * (z-1)) + CorrWindow);
-        tf = find(tim > StepSz * (fakies(z)-1) & tim < (StepSz * (fakies(z)-1)) + CorrWindow);
         
+        % Our time window
+        tt = find(tim > StepSz * (z-1) & tim < (StepSz * (z-1)) + CorrWindow);
+
+        % Our shuffled time window
+        tf = find(tim > StepSz * (fakies(z)-1) & tim < (StepSz * (fakies(z)-1)) + CorrWindow);
         makethemthesamelength = min([length(tt), length(tf)]);
         tt = tt(1:makethemthesamelength);
         tf = tf(1:makethemthesamelength);
         
+        % Our shifted time window
+        if z ~= stepnum
+        tt = find(tim > StepSz * z & tim < (StepSz * z) + CorrWindow);
+        else
+        tt = find(tim > 0 & tim < CorrWindow); % The first window
+        end
+        
         tmp = corrcoef(data(kk).pair(j).descartes(tt), data(kk).pair(j).dF(tt));
             realCorrs(end+1) = tmp(2);
         tmp = corrcoef(data(kk).pair(j).descartes(tt), data(kk).pair(j).dF(tf));
-            fakeCorrs(end+1) = tmp(2);
+            shuffCorrs(end+1) = tmp(2);
+        tmp = corrcoef(data(kk).pair(j).descartes(tt), data(kk).pair(j).dF(tf));
+            shiftCorrs(end+1) = tmp(2);
+            
         clear tmp;
     end
     
