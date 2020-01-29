777601ede50487c568dc11f29081db5a2d515196
Eric Fortune <eric.fortune@njit.edu>
2020-01-29 04:50:56 -0500

    / ‘Brasil/Daphne2019/runDfDist.m’

--- Brasil/Daphne2019/runDfDist.m
+++ Brasil/Daphne2019/runDfDist.m
@@ -92,7 +92,7 @@ pltbins = -0.83335:0.3333:0.83335;
 
 % We have 8 shuffCors greater than 0.8 and 24 realCors > 0.8
 % This is significantly more high positive correlations than expected.
-gtgt(1,:) = [length(find(shuffCorrs > 0.8)), length(find(shuffCorrs < 0.8))];
+gtgt(1,:) = [length(find(shiftCorrs > 0.8)), length(find(shiftCorrs < 0.8))];
 gtgt(2,:) = [length(find(realCorrs > 0.8)), length(find(realCorrs < 0.8))];
 [h,p,stats] = fishertest(gtgt)
 
@@ -101,7 +101,7 @@ gtgt(2,:) = [length(find(realCorrs > 0.8)), length(find(realCorrs < 0.8))];
 % We have 8 shuffCors less than -0.8 and 36 realCors < 0.8
 % This is significantly more high positive correlations than expected.
 % p < 0.0001, Chi Squared = 98.201, 1 degree of freedom (two tailed)
-ltlt(1,:) = [length(find(shuffCorrs < -0.8)), length(find(shuffCorrs > -0.8))];
+ltlt(1,:) = [length(find(shiftCorrs < -0.8)), length(find(shiftCorrs > -0.8))];
 ltlt(2,:) = [length(find(realCorrs < -0.8)), length(find(realCorrs > -0.8))];
 [h,p,stats] = fishertest(ltlt)
 
