897dbab9d9876f209779cdd312f28c782ecc4c07
Eric Fortune <eric.fortune@gmail.com>
2020-02-06 12:25:34 -0500

    / ‘Brasil/Daphne2019/runDfDist.m’

--- Brasil/Daphne2019/runDfDist.m
+++ Brasil/Daphne2019/runDfDist.m
@@ -77,8 +77,8 @@ end % For each entry
 
 %% Plot the histgram data
 
-% figure(1); clf; 
-figure(3); clf;
+figure(1); clf; 
+% figure(3); clf;
 cenbins = -1:0.33333:1;
 pltbins = -0.83335:0.3333:0.83335;
 % ax(1) = subplot(311); histogram(realCorrs, cenbins);
@@ -108,8 +108,8 @@ ltlt(2,:) = [length(find(realCorrs < negthresh)), length(find(realCorrs > negthr
 [h,p,stats] = fishertest(ltlt)
 
 
-% figure(2); clf;
-figure(4); clf;
+figure(2); clf;
+% figure(4); clf;
 
 totalnum = length(realCorrs);
 
