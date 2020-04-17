721552c9328ef1b98b620c7b6813e366e79265f5
Eric Fortune <eric.fortune@njit.edu>
2020-04-17 11:26:41 -0400

    / ‘General/makeGallmanMovie.m’

--- General/makeGallmanMovie.m
+++ General/makeGallmanMovie.m
@@ -110,10 +110,10 @@ end
 gIDX = find(rmsamp ~= 0);
 
 figure(2); clf; 
-    ax(1) = subplot(411); plot(lightims(gIDX), fftamp(gIDX,1), '.-', 'MarkerSize', 8);
-    ax(2) = subplot(412); plot(lightims(gIDX), rmsamp(gIDX), '.-', 'MarkerSize', 8)
-    ax(3) = subplot(413); plot(lightims(gIDX), fftamp(gIDX,2), '.-', 'MarkerSize', 8); ylim([200 700]);
-    ax(4) = subplot(414); plot(lightims(gIDX), lightlevel(gIDX), '.-', 'MarkerSize', 8)
+    ax(1) = subplot(411); plot(lightims(gIDX), fftamp(gIDX,1), '.-', 'MarkerSize', 8); title('FFT amplitude');
+    ax(2) = subplot(412); plot(lightims(gIDX), rmsamp(gIDX), '.-', 'MarkerSize', 8); title('RMS amplitude');
+    ax(3) = subplot(413); plot(lightims(gIDX), fftamp(gIDX,2), '.-', 'MarkerSize', 8); ylim([200 700]); title('EOD Frequency');
+    ax(4) = subplot(414); plot(lightims(gIDX), lightlevel(gIDX), '.-', 'MarkerSize', 8); title('Light level');
     linkaxes(ax, 'x');
     
 % figure(2); clf; 
