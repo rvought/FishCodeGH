ff7083409b420938da0b41a59a43a735cb2e0272
Eric Fortune <eric.fortune@njit.edu>
2020-04-16 21:15:39 -0400

    / ‘General/makeGallmanMovie.m’

--- General/makeGallmanMovie.m
+++ General/makeGallmanMovie.m
@@ -103,6 +103,7 @@ for j=2:length(lightlevelidxs)
 end
 
 
+
 figure(2); clf; 
 subplot(311); plot(lightims, fftamp(:,1), '.', 'MarkerSize', 5);
 subplot(312); plot(lightims, rmsamp, '.', 'MarkerSize', 5)
