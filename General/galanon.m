9f890e664742e30fbbd813e3ac93b04e22dba725
Eric Fortune <eric.fortune@njit.edu>
2020-05-04 14:12:00 -0400

    / ‘General/galanon.m’

--- General/galanon.m
+++ General/galanon.m
@@ -143,7 +143,7 @@ end
 figure(2); clf; 
     ax(1) = subplot(411); 
         yyaxis right; plot(dataidxs, fftCh1(:,1), 'b.-', 'MarkerSize', 8); title('FFT amplitude');
-        hold on; yyaxis left; plot(dataidxs, fftCh2(:,1), 'm.-', 'MarkerSize', 8); 
+        hold on; yyaxis(ax1, left; plot(dataidxs, fftCh2(:,1), 'm.-', 'MarkerSize', 8); 
     ax(2) = subplot(412); 
         yyaxis right; plot(dataidxs, rmsCh1, 'b.-', 'MarkerSize', 8);
         hold on; yyaxis left; plot(dataidxs, rmsCh2, 'm.-', 'MarkerSize', 8); title('RMS amplitude');
