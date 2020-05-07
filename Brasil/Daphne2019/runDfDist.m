98a7f87332a352e0572ab107a56a7763bf45cd71
Eric Fortune <eric.fortune@njit.edu>
2020-05-07 11:03:09 -0400

    / ‘Brasil/Daphne2019/runDfDist.m’

--- Brasil/Daphne2019/runDfDist.m
+++ Brasil/Daphne2019/runDfDist.m
@@ -137,6 +137,9 @@ totalnum = length(realCorrs);
 
 
 [h,p,ci,stats] = vartest2(realCorrs, shuffCorrs);
+fprintf('vartest2 realCorrs versus shuffCorss p=%1.6f', p);
+
+fprintf('Mean dF=%3.4f and var=%3.4f and N=%i\n', mean(meandF), var(meandF), length(meandF));
 
 clear stepnum tt tf ts fakies z tim makethemthesamelength numbins j kk cenbins
 
