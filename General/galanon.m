31a979da41dfc2f3272cd76472b211099106a8ac
Eric Fortune <eric.fortune@njit.edu>
2020-05-11 16:30:49 -0400

    / ‘General/galanon.m’

--- General/galanon.m
+++ General/galanon.m
@@ -146,7 +146,7 @@ f2 = figure(2); clf;
     bb = [0 0 1]; mm = [1 0 1];
     set(f2, 'defaultAxesColorOrder', [bb; mm]);
     ax(1) = subplot(411); 
-        for kk = 1:length(out); fftCh1tmp(kk) = out(kk).fftCh1.fftpeakamp; fftCh2tmp(kk) = out(kk).fftCh2.fftpeakamp;
+        for kk = 1:length(out); fftCh1tmp(kk) = out(kk).fftCh1.fftpeakamp; fftCh2tmp(kk) = out(kk).fftCh2.fftpeakamp; end
         yyaxis left; plot([out.idx], fftCh1tmp, 'b.-', 'MarkerSize', 8); title('FFT amplitude');
 %        yyaxis left; plot([out.idx], [out.fftCh1.fftpeakamp], 'b.-', 'MarkerSize', 8); title('FFT amplitude');
         hold on; 
