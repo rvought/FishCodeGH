ebd1a523fc7eb7a1ad14e0f8a40e844a79bf708a
Eric Fortune <eric.fortune@njit.edu>
2020-05-14 17:28:06 -0400

    / ‘General/galanon.m’

--- General/galanon.m
+++ General/galanon.m
@@ -67,11 +67,12 @@ while eidx <= length(eFiles)
                 
                 [minVar(j), minVaridx] = min(totalvar);
                 minVartim(j) = tim(tt(1)) + stepsize*(minVaridx-1);
+                [R, minVaridx] = sort(minVar(j));
                 
                 plot([minVartim(j), minVartim(j)], [-1.5, 1.5], 'k-', 'LineWidth', 2);
                 plot([minVartim(j)+winwidth, minVartim(j)+winwidth], [-1.5, 1.5], 'k-', 'LineWidth', 2); 
                 
-                text(8+((j-1)*10), 1, sort(minVar(j)), 'Color', 'k', 'FontSize', 24);
+                text(8+((j-1)*10), 1, R, 'Color', 'k', 'FontSize', 24);
                 %text(8+((j-1)*10), 1, num2str(minVar(j)), 'Color', 'k', 'FontSize', 24);
                 
     end
