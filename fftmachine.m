3e194ec9ce79eda16322f834134369a640e23cbc
Eric Fortune <eric.fortune@njit.edu>
2020-05-18 13:26:40 -0400

    / ‘fftmachine.m’

--- fftmachine.m
+++ fftmachine.m
@@ -13,9 +13,9 @@ end
 
 L = length(data);
 
-% NFFT = 2^nextpow2(L); % Next power of 2 from length of the data
+NFFT = 2^nextpow2(L); % Next power of 2 from length of the data
 
-NFFT = 1024*2;
+% NFFT = 1024*2;
 
 fftdata = fft(data,NFFT)/L;
 % f = Fs/2*linspace(0,1,NFFT/2+1);
