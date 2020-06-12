9c3c8d2c49567e584f23a78f6f9293c36e10d20a
Eric Fortune <eric.fortune@njit.edu>
2020-06-12 16:02:31 -0400

    / ‘General/concatonator.m’

--- General/concatonator.m
+++ General/concatonator.m
@@ -58,13 +58,13 @@ figure(4); clf;
 
 ax(1)=subplot(311); 
     plot(c.idx, c.fftCh1amp, 'LineWidth', 4); 
-    hold on; plot(c.idx, c.fftCh2amp, 'LineWidth', 4);
+    hold on; plot(c.timmy(c.idx), c.fftCh2amp, 'LineWidth', 4);
 
 ax(2)=subplot(312); 
     plot(c.idx, c.rmsCh1, 'LineWidth', 4); 
-    hold on; plot(c.idx, c.rmsCh2, 'LineWidth', 4);
+    hold on; plot(c.timmy(c.idx), c.rmsCh2, 'LineWidth', 4);
 
-ax(3)=subplot(313); plot(1:length(c.lightlevel), c.lightlevel, 'LineWidth', 4);
+ax(3)=subplot(313); plot(c.timmy, c.lightlevel, 'LineWidth', 4);
 linkaxes(ax, 'x');
 
 
