48599f9789eb6c7475bfeb3fe6625c578c2c64e8
Eric Fortune <eric.fortune@njit.edu>
2020-06-12 22:42:25 -0400

    / ‘General/rotatepoints.m’

--- General/rotatepoints.m
+++ General/rotatepoints.m
@@ -9,42 +9,46 @@ function out = rotatepoints(dat, deg)
 
 
 %% Prepare data
+
 % STRATEGY 1: Calculate centroid for center of rotation
 
     poly = polyshape(dat); % Change the data into a Matlab object known as a polyshape
-    [xC,yC] = centroid(poly); % centroid is a useful command that can calculate a variety of centroid variants.
+    [xC,yC] = centroid(poly); % centroid calculates centroids.
     
 % STRATEGY 2: Take the third point as the center of rotation
 
-    tt = dat(3,:);    
+    tt = dat(3,:);  % Just take the third XY value in the array. Why not(?), unless you have fewer than three points.
     
 % Convert degrees to radians 
 
     rad = deg2rad(deg);
 
-%% Rotate!
+%% Rotate! Using the embedded function below, rotatorcuff
 
     out.deg = deg; % Copy of rotation for your records
-    out.centroidrotate = rotatorcuff(dat, [xC,yC], rad);
-    out.thirdrotate = rotatorcuff(dat, tt, rad);
+    out.centroidrotate = rotatorcuff(dat, [xC,yC], rad); % Rotation around centroid 
+    out.thirdrotate = rotatorcuff(dat, tt, rad); % Rotation around 3rd XY value
     
-%% Plot
+%% Plot the results
+
 figure(1); clf;
 
 ax(1) = subplot(121); title('Centroid'); hold on; 
-    plot(dat(:,1), dat(:,2), 'b-*'); % Original data
-    plot(out.centroidrotate(:,1), out.centroidrotate(:,2), 'g-*'); 
-    plot(xC, yC, 'r*');
+    plot(dat(:,1), dat(:,2), 'b-*'); % Original data in blue
+    plot(out.centroidrotate(:,1), out.centroidrotate(:,2), 'g-*'); % Rotated data in green
+    % out.centroidrotate(:,1) are the X values, out.centroidrotate(:,2) are Ys
+    plot(xC, yC, 'r*'); % Red dot for the center
 
 ax(2) = subplot(122); title('Third'); hold on; 
-    plot(dat(:,1), dat(:,2), 'b-*'); % Original data
-    plot(out.thirdrotate(:,1), out.thirdrotate(:,2), 'g-*'); 
-    plot(tt(1),tt(2), 'r*');
+    plot(dat(:,1), dat(:,2), 'b-*'); % Original data in blue
+    plot(out.thirdrotate(:,1), out.thirdrotate(:,2), 'g-*'); % Rotated data in green
+    plot(tt(1),tt(2), 'r*'); % Red dot for the center
 
 linkaxes(ax, 'xy'); 
 
 
-%% Embedded rotation function
+%% Embedded rotation function (You could make this a separate function)
+
 function rot = rotatorcuff(data, cent, degR)
 % data is the x,y values
 % cent is the rotation center [x,y]
