fa8778f570101690d50c047b32390f49b33c92e6
Eric Fortune <eric.fortune@gmail.com>
2020-06-15 11:32:30 -0400

    / ‘Ismail/iu_sta.m’

--- Ismail/iu_sta.m
+++ Ismail/iu_sta.m
@@ -1,15 +1,20 @@
 function out = iu_sta(spikes, randspikes, sig, Fs, wid)
 
-tim = 1/Fs:1/Fs:length(sig)/Fs; % Time
+tim = 1/Fs:1/Fs:length(sig)/Fs; % Time stamps for the duration of the signal.
 
+% For every spike (starting at the end) get the time "wid" before and after
+% the time of the spike.
 for idx = length(spikes):-1:1
-    if spikes(idx) > wid && spikes(idx) < tim(end)-wid
-        temp = interp1(tim, sig, spikes(idx)-wid:1/Fs:spikes(idx)+wid);
-        sta(idx,:) = temp;
+    if spikes(idx) > wid && spikes(idx) < tim(end)-wid % Make sure that the window does not go before or after the signal.
+        temp = interp1(tim, sig, spikes(idx)-wid:1/Fs:spikes(idx)+wid); % Copy the signal 
+        sta(idx,:) = temp; % Put the signal into a temporary structure
     end
 end
+
+% For every randome spike (starting at the end) get the time "wid" before and after
+% the time of the spike.
 for idx = length(randspikes):-1:1
-    if randspikes(idx) > wid && randspikes(idx) < tim(end)-wid
+    if randspikes(idx) > wid && randspikes(idx) < tim(end)-wid % Make sure that the window does not go before or after the signal.
         temp = interp1(tim, sig, randspikes(idx)-wid:1/Fs:randspikes(idx)+wid);    
         sta_rand(idx,:) = temp;
     end
