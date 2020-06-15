b80dcc69ee42e3b104f8b3a027d76db32f51319c
Eric Fortune <eric.fortune@njit.edu>
2020-06-15 07:55:50 -0400

    + ‘Ismail/iu_PlotSTA.m’ + ‘Ismail/iu_sta.m’

---
+++ Ismail/iu_PlotSTA.m
@@ -0,0 +1,62 @@
+% Plot spike triggered averages for error_pos, error_vel, error_acc, and error_jerk
+% Load data first! Relies on iu_sta.m
+
+
+%% Calculate spike triggered averages
+    fprintf('Calculating error_pos STA.\n');
+    epos = iu_sta(spikes.times, spikes_rand.times, error_pos, fs, 2);
+    fprintf('Calculating error_vel STA.\n');
+    evel = iu_sta(spikes.times, spikes_rand.times, error_vel, fs, 2);
+    fprintf('Calculating error_acc STA.\n');
+    eacc = iu_sta(spikes.times, spikes_rand.times, error_acc, fs, 2);
+    fprintf('Calculating error_jerk STA.\n');
+    ejerk = iu_sta(spikes.times, spikes_rand.times, error_jerk, fs, 2);
+
+%% Plot them all in one figure
+    figure(1); clf; 
+
+    subplot(221); title('Position'); hold on;
+    plot([0, 0], [min(epos.MEAN), max(epos.MEAN)], 'k-', 'LineWidth',1);
+    plot(epos.time, epos.MEAN, 'b-', 'LineWidth', 3);
+    plot(epos.time, epos.randMEAN,'r-','LineWidth',3);
+
+    subplot(222); title('Acceleration'); hold on;
+    plot([0, 0], [min(eacc.MEAN), max(eacc.MEAN)], 'k-', 'LineWidth',1);
+    plot(eacc.time, eacc.MEAN, 'b-', 'LineWidth', 3);
+    plot(eacc.time, eacc.randMEAN,'r-','LineWidth',3);
+
+    subplot(223); title('Velocity'); hold on;
+    plot([0, 0], [min(evel.MEAN), max(evel.MEAN)], 'k-', 'LineWidth',1);
+    plot(evel.time, evel.MEAN, 'b-', 'LineWidth', 3);
+    plot(evel.time, evel.randMEAN,'r-','LineWidth',3);
+    
+    subplot(224); title('Jerk'); hold on;
+    plot([0, 0], [min(ejerk.MEAN), max(ejerk.MEAN)], 'k-', 'LineWidth',1);
+    plot(ejerk.time, ejerk.MEAN, 'b-', 'LineWidth', 3);
+    plot(ejerk.time, ejerk.randMEAN,'r-','LineWidth',3);
+
+%% Plot the error
+
+figure(2); clf; 
+
+    subplot(221); title('Position'); hold on;
+    plot([0, 0], [min(epos.STD), max(epos.STD)], 'k-', 'LineWidth',1);
+    plot(epos.time, epos.STD, 'b-', 'LineWidth', 3);
+    plot(epos.time, epos.randSTD, 'r-', 'LineWidth', 3);
+
+    subplot(222); title('Acceleration'); hold on;
+    plot([0, 0], [min(eacc.STD), max(eacc.STD)], 'k-', 'LineWidth',1);
+    plot(eacc.time, eacc.STD, 'b-', 'LineWidth', 3);
+    plot(eacc.time, eacc.randSTD, 'r-', 'LineWidth', 3);
+
+    subplot(223); title('Velocity'); hold on;
+    plot([0, 0], [min(evel.STD), max(evel.STD)], 'k-', 'LineWidth',1);
+    plot(evel.time, evel.STD, 'b-', 'LineWidth', 3);
+    plot(evel.time, evel.randSTD, 'r-', 'LineWidth', 3);
+
+    subplot(224); title('Jerk'); hold on;
+    plot([0, 0], [min(ejerk.STD), max(ejerk.STD)], 'k-', 'LineWidth',1);
+    plot(ejerk.time, ejerk.STD, 'b-', 'LineWidth', 3);
+    plot(ejerk.time, ejerk.randSTD, 'r-', 'LineWidth', 3);
+
+    
---
+++ Ismail/iu_sta.m
@@ -0,0 +1,34 @@
+function out = iu_sta(spikes, randspikes, sig, Fs, wid)
+
+tim = 1/Fs:1/Fs:length(sig)/Fs; % Time
+
+for idx = length(spikes):-1:1
+    if spikes(idx) > wid && spikes(idx) < tim(end)-wid
+        temp = interp1(tim, sig, spikes(idx)-wid:1/Fs:spikes(idx)+wid);
+        sta(idx,:) = temp;
+    end
+end
+for idx = length(randspikes):-1:1
+    if randspikes(idx) > wid && randspikes(idx) < tim(end)-wid
+        temp = interp1(tim, sig, randspikes(idx)-wid:1/Fs:randspikes(idx)+wid);    
+        sta_rand(idx,:) = temp;
+    end
+end
+
+    out.MEAN  = nanmean(sta,1);
+    out.STD  = nanstd(sta,0,1);
+
+    out.randMEAN  = nanmean(sta_rand,1);
+    out.randSTD  = nanstd(sta_rand,0,1);
+
+    out.time = -wid:1/Fs:wid;
+
+% figure,
+% hold on,
+% box on,
+%     plot([0, 0], [min(out.MEAN), max(out.MEAN)], 'k-', 'LineWidth',1);
+%     plot(out.time, out.MEAN, 'b-', 'LineWidth', 3);
+%     plot(out.time, out.randMEAN,'r-','LineWidth',3);
+%     xlabel('Time (s)')
+
+ 
