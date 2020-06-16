5360bab9026f42b74cf10b855632c914081e0c50
rvought <67012292+rvought@users.noreply.github.com>
2020-06-16 17:35:52 -0400

    Update rita1stassignment.m

--- Ismail/rita1stassignment.m
+++ Ismail/rita1stassignment.m
@@ -51,6 +51,60 @@ ismail(j).shuttle_pos = shuttle_pos;
 ismail(j).shuttle_vel = shuttle_vel;
 ismail(j).shuttle_acc = shuttle_acc; 
 
+%% 9_25_2019_part1
+j=3;
+load /Volumes/Cgate/Data2019/9_25_2019/Brown2018_09_25_merged_part1_ID2513.mat
+
+ismail(j).date=09252019;
+ismail(j).filename='9_25_2019/Brown2018_09_25_merged_part1_ID2513.mat';
+
+ismail(j).Fs = fs;
+ismail(j).time = time;         
+
+ismail(j).spikes = spikes;
+ismail(j).spikes_rand = spikes_rand;
+
+ismail(j).fish_pos = fish_pos;
+ismail(j).fish_acc = fish_acc;
+ismail(j).fish_vel = fish_vel;
+ismail(j).fish_jerk = fish_jerk;
+
+ismail(j).error_pos = error_pos;    
+ismail(j).error_vel = error_vel;
+ismail(j).error_acc = error_acc;
+ismail(j).error_jerk = error_jerk;
+
+ismail(j).shuttle_pos = shuttle_pos;
+ismail(j).shuttle_vel = shuttle_vel;
+ismail(j).shuttle_acc = shuttle_acc; 
+
+%% 9_25_2019_part2
+j=4;
+load /Volumes/Cgate/Data2019/9_25_2019/Brown2018_09_25_merged_part2_ID1476.mat
+
+ismail(j).date=09252019;
+ismail(j).filename='9_25_2019/Brown2018_09_25_merged_part2_ID1476.mat';
+
+ismail(j).Fs = fs;
+ismail(j).time = time;         
+
+ismail(j).spikes = spikes;
+ismail(j).spikes_rand = spikes_rand;
+
+ismail(j).fish_pos = fish_pos;
+ismail(j).fish_acc = fish_acc;
+ismail(j).fish_vel = fish_vel;
+ismail(j).fish_jerk = fish_jerk;
+
+ismail(j).error_pos = error_pos;    
+ismail(j).error_vel = error_vel;
+ismail(j).error_acc = error_acc;
+ismail(j).error_jerk = error_jerk;
+
+ismail(j).shuttle_pos = shuttle_pos;
+ismail(j).shuttle_vel = shuttle_vel;
+ismail(j).shuttle_acc = shuttle_acc; 
+
 
 % save filename.mat ismail
 
